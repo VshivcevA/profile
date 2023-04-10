212576aa6a418841fea263ca8c2b9940f1e5cd99
VshivcevA <vshivcev.alexandr@gmail.com>
2023-04-10 11:53:14 +0300

    repos sort by created_at

--- src/components/git/GitRepositories.js
+++ src/components/git/GitRepositories.js
@@ -36,21 +36,24 @@ function GitRepositories() {
           description={"Задачи на Vanilla JS c codewars.com"}
           giturl={"https://github.com/VshivcevA/CodeWars"}
         />
+
         {/* eslint-disable-next-line array-callback-return */}
-        {gitRepos.map((repo) => {
-          if (repo.homepage) {
-            return (
-              <ImgMediaCard
-                key={repo.id}
-                name={repo.name}
-                description={repo.description}
-                giturl={repo.html_url}
-                depurl={repo.homepage}
-                topics={repo.topics}
-              />
-            );
-          }
-        })}
+        {gitRepos
+          .sort((a, b) => (a.created_at < b.created_at ? 1 : -1))
+          .map((repo) => {
+            if (repo.homepage) {
+              return (
+                <ImgMediaCard
+                  key={repo.id}
+                  name={repo.name}
+                  description={repo.description}
+                  giturl={repo.html_url}
+                  depurl={repo.homepage}
+                  topics={repo.topics}
+                />
+              );
+            }
+          })}
       </div>
     );
   }
